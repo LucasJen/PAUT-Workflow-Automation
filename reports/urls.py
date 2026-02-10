8aa3270231f9e273f98d5d538b06cffd6064f083
LucasJen <fake@fake.com>
2026-02-10 11:06:16 -0600

    added create hic report landing page

--- reports/urls.py
+++ reports/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/hic/', views.create_hic_report, name='create_hic_report'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 def home(request):
@@ -8,3 +9,18 @@ def home(request):
     }
     return render(request, 'reports/home.html', context)
 
+def create_hic_report(request):
+    """Create HIC Report - Long form"""
+    
+    if request.method == 'POST':
+        # We'll handle form submission later
+        # For now, just acknowledge it
+        print("Form submitted!")
+        # TODO: Process form data
+        pass
+    
+    # GET request - show empty form
+    context = {
+        'page_title': 'Create HIC Report',
+    }
+    return render(request, 'reports/create_hic.html', context)
