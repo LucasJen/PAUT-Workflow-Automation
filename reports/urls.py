83d43d5b3f166001c6cd7c7394e97f284da1dace
LucasJen <fake@fake.com>
2026-02-12 07:02:16 -0600

    removed django template

--- reports/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('create/hic/', views.create_hic_report, name='create_hic_report'),
-]
