92716f559234b78d73174b4e474dc33a94096440
LucasJen <fake@fake.com>
2026-02-12 09:06:06 -0600

    added form to retrieve technician name

--- reports/views.py
+++ reports/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+
+    if request.method == 'POST':
+        # Extract the technician name from the form
+        technician_name = request.POST.get('technician_name')
+        
+        # Print to terminal (console)
+        print("=" * 50)
+        print("FORM SUBMITTED!")
+        print(f"Technician Name: {technician_name}")
+        print("=" * 50)
+
     return render(request, 'reports/home.html')
