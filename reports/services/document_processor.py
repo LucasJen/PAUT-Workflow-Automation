a805432eebfdc343a01785b7adef4c59b64d37c8
LucasJen <fake@fake.com>
2026-02-12 17:04:26 -0600

    Fixed loop and save bug in document_processor

--- reports/services/document_processor.py
+++ reports/services/document_processor.py
@@ -8,19 +8,21 @@ class WordTemplateProcessor:
         self.document = Document(template_path)
 
     def replace_text_in_paragraphs(self, paragraph, placeholder, replacement):
-        for placeholder in paragraph.text:
+        if placeholder in paragraph.text:
             for run in paragraph.runs:
                 if placeholder in run.text:
                     run.text = run.text.replace(placeholder, replacement)
+                    print("reeeee")
     
     def replace(self, placeholder, replacement):
 
         # Search for matching placeholders with the text of the document.
-
+        print("Searching Paragraphs")
         # Search Paragraphs
         for paragraph in self.document.paragraphs:
             self.replace_text_in_paragraphs(paragraph, placeholder, replacement)
 
+        print("searching tables")
         # Search Tables
         for table in self.document.tables:
             for row in table.rows:
@@ -31,6 +33,6 @@ class WordTemplateProcessor:
 
     def save(self):
         os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
-        self.document.save(self.output)
+        self.document.save(self.output_path)
 
 
--- reports/views.py
+++ reports/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render
 from django.conf import settings
-from .services import document_processor
+from .services.document_processor import WordTemplateProcessor
 import os
 
 def home(request):
     if request.method == 'POST':
         technician_name = request.POST.get('technician_name')
         
-        print("FORM SUBMITTED!")
-        print(f"Technician Name: {technician_name}")
+        # print("FORM SUBMITTED!")
+        # print(f"Technician Name: {technician_name}")
         
-        # Define paths
-        template_path = os.path.join(settings.BASE_DIR, 'word_templates', 'long_form_template.docx')
-        output_path = os.path.join(settings.BASE_DIR, 'outputs', 'report_output.docx')
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        # Define paths for the input document and output location.
+
+        #input template
+        template_path = os.path.join(
+            settings.BASE_DIR, 'word_templates', 'long_form_template.docx'
+        )
+        print(template_path)
+        # output product
+        output_path = os.path.join(
+            settings.BASE_DIR, 'outputs', 'report_output.docx'
+        )
+        print(output_path)
         
         # Open and modify document
-        doc = Document(template_path)
-        replace_in_document(doc, '{{TECHNICIAN_NAME}}', technician_name)
-        
-        # Save
-        doc.save(output_path)
-        print(f"Saved output to: {output_path}")
+        processor = WordTemplateProcessor(template_path, output_path)
+        print('object created')
+        processor.replace("{{TECHNICIAN_NAME}}", technician_name)
+        print('replacer ran')
+        #save changes to output location
+        processor.save()
+        print('document saved')
+
         
     return render(request, 'reports/home.html')
