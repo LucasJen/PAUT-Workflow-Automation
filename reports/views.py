f249612c0007903f12ce4dbfe8bfe12776227337
LucasJen <fake@fake.com>
2026-02-24 15:38:29 -0600

    add config dicts

--- reports/views.py
+++ reports/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render
 from django.conf import settings
 from .services.document_processor import WordTemplateProcessor
+from .services.document_config import DOCUMENT_CONFIGS
 import os
 
 def home(request):
     if request.method == 'POST':
-        # List of all user inputs to be replaced: 
-        technician_name = request.POST.get('technician_name')
-       
-        print("FORM SUBMITTED!")
-        print(f"Technician Name: {technician_name}")
+        report_type = "long_form"
+        config = DOCUMENT_CONFIGS[report_type]
 
-        #input template
         template_path = os.path.join(
             settings.BASE_DIR, 'word_templates', 'long_form_template.docx'
         )
-        print(template_path)
-        # output product
+
         output_path = os.path.join(
             settings.BASE_DIR, 'outputs', 'report_output.docx'
         )
-        print(output_path)
+
+        replacements = {
+            f"{{{{{field.upper()}}}}}": request.POST.get(field)
+            for field in config["LONG_FORM_FIELDS"]
+        }
+
+        print(replacements)
+        # List of all user inputs to be replaced: 
+
+
+        technician_name = request.POST.get('technician_name')
+       
+
+
         
 
         # # Open and modify document
