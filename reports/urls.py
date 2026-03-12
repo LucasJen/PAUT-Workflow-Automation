9790fc6d93072b7da4654c599a77d6e3e7234ff5
LucasJen <fake@fake.com>
2026-03-12 10:12:22 -0500

    updated url names

--- reports/urls.py
+++ reports/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create_report, name='create-report'),
     path('setups/', views.setup_list, name='setup-list'),
-    path('setup/<int:pk>/edit/', views.edit_setups, name='edit-setups'),
-    path('report list/', views.report_list, name='report-list')
+    path('setup/<int:pk>/edit/', views.edit_setup, name='edit-setup'),
+    path('reports/', views.report_list, name='report-list')
 ]
