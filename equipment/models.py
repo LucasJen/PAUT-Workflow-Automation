45f0196571d85811f93681e804a73ebc2a229f92
LucasJen <fake@fake.com>
2026-04-28 17:05:23 -0500

    added more fields for scopes

--- equipment/models.py
+++ equipment/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 class Scope(models.Model):
     manufacturer = models.CharField(max_length=200, blank=True, default='Evident')
-    platform = models.CharField(max_length=200, blank=True)
+    software = models.CharField(max_length=200, blank=True, default='OmniPC')
+    software_version = models.CharField(max_length=100, blank=True)
     model = models.CharField(max_length=200, blank=True)
     serial_number = models.CharField(max_length=200, blank=True)
+    calibration_date = models.DateField(blank=True, null=True)
+    calibration_due_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.model} ({self.serial_number})"
